71d29978ceffd0c2dd085b40f3d91579d5602d22
ZER-DAY <bahaa.ali.m.alhabil@gmail.com>
2025-02-02 00:47:42 +0300

    bahaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 // src/app/page.tsx
+import { Analytics } from "@vercel/analytics/react";
 
 import PostsList from "@/sections/PostsList";
 import SwiperSection from "@/sections/SwiperSection";
 export default function HomePage() {
   return (
     <>
+      <Analytics />
       <SwiperSection />
       <PostsList />
     </>
